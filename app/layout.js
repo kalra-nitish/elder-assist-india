94dedb57ca1e5ad8a2adf3f2e193229b74afbc1f
Nitish Kalra <nitishkalra2618@gmail.com>
2025-12-30 22:21:16 +0530

    all set

--- app/layout.js
+++ app/layout.js
@@ -17,13 +17,6 @@ export default function RootLayout({ children }) {
             <GoogleAnalytics />
             <body>{children}</body>
         </html>
-         <html lang="en" className={`${poppins.variable} ${pT_Serif.variable}`}>
-         <GoogleAnalytics />
-         <body>{children}</body>
-     </html>
-      <html lang="en" className={`${poppins.variable} ${pT_Serif.variable}`}>
-      <GoogleAnalytics />
-      <body>{children}</body>
-  </html>
+        
     )
 }
